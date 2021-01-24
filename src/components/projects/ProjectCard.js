2e088c843c65f4b2224e354436937927ce1cea03
onlytruejeremy <jeremydev2020@gmail.com>
2021-01-24 05:42:55 -0800

    Prevented touch on carousel

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -6,11 +6,20 @@ const ProjectCard = (props) => {
     const element = document.getElementsByClassName(
       "projects__carousel__inner"
     );
-    const touchPrevention = (e) => {
-      e.preventDefault();
+    let touch;
+    const touchPreventionStart = (e) => {
+      touch = e.originalEvent.touches[0].clientX;
     };
-    element[0].addEventListener("touchstart", touchPrevention);
-    element[0].addEventListener("touchmove", touchPrevention);
+    const touchPrevent = (e) => {
+      let touchEnd = e.originalEvent.changedTouches[0].clientX;
+      if (touch > touchEnd) {
+        e.preventDefault();
+      } else if (touch < touchEnd) {
+        e.preventDefault();
+      }
+    };
+    element[0].addEventListener("touchstart", touchPreventionStart);
+    element[0].addEventListener("touchmove", touchPrevent);
   }, []);
   return (
     <div className="card">
