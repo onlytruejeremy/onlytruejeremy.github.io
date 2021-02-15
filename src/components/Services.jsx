160ac039155277c64df622d91cf1eb8b3068f8a4
onlytruejeremy <jeremydev2020@gmail.com>
2021-02-15 04:16:48 -0800

    Added info

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -11,8 +11,8 @@ const Services = (props) => {
       paragraph:
         "We'll work together to produce an experience your customers will love.",
       tech: `HTML5, CSS3, Bootstrap,
-              JavaScript, jQuery, ReactJS, JSON, Formik,
-              Yup
+              JavaScript, jQuery, ReactJS, Formik,
+              Yup, Axios
               `,
       imgUrl:
         "https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -21,9 +21,8 @@ const Services = (props) => {
       id: 2,
       title: "Middle Tier",
       paragraph:
-        "Enhancing an existing API or building something new? I've got the skills for both.",
-      tech: `NodeJS, Tedious, Mongoose,
-              MySQL, Joi, JWT, BCRYPT
+        "I've got the skills necessary for building out new document-based and SQL-based databases.",
+      tech: `NodeJS, ExpressJS, Joi, JWT, BCRYPT, CORS, Helmet
               `,
       imgUrl:
         "https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
@@ -33,29 +32,12 @@ const Services = (props) => {
       title: "Back End",
       paragraph:
         "Document-based or SQL-based database? Either way I'm ready to tackle the challenge.",
-      tech: `MongoDB, FireBase, MSSQL,
-              and MySQL
+      tech: `MongoDB, FireBase, MSSQL, MySQL
               `,
       imgUrl:
         "https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
     },
   ];
-  /*   React.useEffect(() => {
-    const adjustHeight = () => {
-      let cards = document.getElementsByClassName("service__card");
-      let finalHeight = 0;
-      for (let i = 0; i < cards.length; i++) {
-        const current = cards[i];
-        let height = current.getBoundingClientRect().height;
-
-        if (height >= finalHeight) {
-          finalHeight = height;
-        }
-        current.style.height = `${finalHeight}px`;
-      }
-    };
-    adjustHeight();
-  }, [data]); */
 
   return (
     <Layout>
